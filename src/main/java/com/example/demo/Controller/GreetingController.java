f3f9148b6590a33b1bdce2e7756bc5d57b7753f0
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-03-03 14:20:42 +0530

    Added:firstName and lastName based on user attributes

--- src/main/java/com/example/demo/Controller/GreetingController.java
+++ src/main/java/com/example/demo/Controller/GreetingController.java
@@ -1,37 +1,52 @@
-package com.example.demo.controller;
+package com.example.demo.Controller;
 
 import com.example.demo.model.Greeting;
 import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
-    // Example: GET http://localhost:8080/greeting/?name=Ayushi
+    // Example: GET http://localhost:8080/greeting/
+    // Example: GET http://localhost:8080/greeting/?firstName=John
+    // Example: GET http://localhost:8080/greeting/?lastName=Doe
+    // Example: GET http://localhost:8080/greeting/?firstName=John&lastName=Doe
     @GetMapping("/")
-    public Greeting getGreeting(@RequestParam(name = "name") String name) {
-        return new Greeting("Hello, " + name + " from GET method!");
+    public Greeting getGreeting(
+            @RequestParam(name = "firstName", required = false) Optional<String> firstName,
+            @RequestParam(name = "lastName", required = false) Optional<String> lastName) {
+
+        String message;
+
+        if (firstName.isPresent() && lastName.isPresent()) {
+            message = "Hello, " + firstName.get() + " " + lastName.get() + "!";
+        } else if (firstName.isPresent()) {
+            message = "Hello, " + firstName.get() + "!";
+        } else if (lastName.isPresent()) {
+            message = "Hello, " + lastName.get() + "!";
+        } else {
+            message = "Hello, World!";
+        }
+
+        return new Greeting(message);
     }
 
-    // Example: POST http://localhost:8080/greeting/
-    // Header: Content-Type: application/json
-    // Body: {"message": "Hello, Spring!"}
+    // Existing POST method
     @PostMapping("/")
     public Greeting postGreeting(@RequestBody Greeting request) {
         String message = request.getMessage() != null ? request.getMessage() : "No message provided";
         return new Greeting("Received message: " + message);
     }
 
-    // Example: PUT http://localhost:8080/greeting/
-    // Header: Content-Type: application/json
-    // Body: {"message": "Updated greeting!"}
+    // Existing PUT method
     @PutMapping("/")
     public Greeting putGreeting(@RequestBody Greeting request) {
         String updatedMessage = request.getMessage() != null ? request.getMessage() : "No updated message provided";
         return new Greeting("Greeting updated to: " + updatedMessage);
     }
 
-    // Example: DELETE http://localhost:8080/greeting/?id=123
+    // Existing DELETE method
     @DeleteMapping("/")
     public Greeting deleteGreeting(@RequestParam(name = "id") int id) {
         return new Greeting("Greeting with ID " + id + " has been deleted.");
